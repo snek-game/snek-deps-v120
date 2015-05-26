8d0c169a1789d7897e41d0a24e442c5b4aa13255
Pranjal Raihan <pranjal@grimpanda.com>
2015-05-26 15:40:01 -0400

    Fix corrupt libboost_atomic zip (x86 debug)

--- extract.py
+++ extract.py
@@ -1,24 +1,36 @@
-import sys, os, fnmatch, zipfile, multiprocessing
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import fnmatch
+import zipfile
+import multiprocessing
 
 EXEC_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 def extract_zip(file_path):
-  with zipfile.ZipFile(file_path, 'r') as z:
-    z.extractall(os.path.dirname(file_path))
-    print 'Extracted', os.path.relpath(file_path, EXEC_DIR)
+    with zipfile.ZipFile(file_path, 'r') as z:
+        z.extractall(os.path.dirname(file_path))
+        print 'Extracted', os.path.relpath(file_path, EXEC_DIR)
+
 
 if __name__ == '__main__':
-  
-  sys.path.append(os.path.abspath(os.path.join(EXEC_DIR, '..', '..', 'tools', 'pytools')))
 
-  from joblib import Parallel, delayed
+    sys.path.append(os.path.abspath(os.path.join(EXEC_DIR, '..', '..',
+                    'tools', 'pytools')))
 
-  os.chdir(EXEC_DIR)
+    from joblib import Parallel, delayed
 
-  matches = []
-  for folder in ['bin', 'lib']:
-    for root, dirnames, filenames in os.walk(folder):
-      for filename in fnmatch.filter(filenames, '*.zip'):
-        matches.append(os.path.join(root, filename))
+    os.chdir(EXEC_DIR)
 
-  Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extract_zip)(f) for f in matches)
+    matches = []
+    for folder in ['bin', 'lib']:
+        for (root, dirnames, filenames) in os.walk(folder):
+            for filename in fnmatch.filter(filenames, '*.zip'):
+                matches.append(os.path.join(root, filename))
+
+    Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extract_zip)(f)
+            for f in matches)
+
+      
